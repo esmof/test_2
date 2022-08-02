bd2b7c4344df34fb64da02bdd70c29f302a36838
esmof <moyinoluwafaleye77@gmail.com>
2022-08-02 10:09:40 -0700

    feat: add first commit

---
+++ 2ndAugTraining.js
@@ -0,0 +1,10 @@
+var myName = 'moyinoluwa';
+
+//print out myName
+//console.log(myName);
+
+//Finding a remainder
+
+var mangoes = 5;
+var remainder = mangoes%3;
+console.log("remainder", remainder);
